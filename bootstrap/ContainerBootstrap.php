2962d0c7e5a020c62bd06d2b68c45953d796420c
ElisDN <mail@elisdn.ru>
2017-04-04 17:35:44 +0300

    Installed Doctrine

--- bootstrap/ContainerBootstrap.php
+++ bootstrap/ContainerBootstrap.php
@@ -4,14 +4,43 @@ namespace app\bootstrap;
 
 use app\dispatchers\EventDispatcher;
 use app\dispatchers\DummyEventDispatcher;
+use app\doctrine\listeners\TablePrefixSubscriber;
+use Doctrine\Common\Cache\ArrayCache;
+use Doctrine\Common\Cache\FilesystemCache;
+use Doctrine\Common\EventManager;
+use Doctrine\ORM\Configuration;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\Mapping\Driver\SimplifiedYamlDriver;
+use Yii;
 use yii\base\BootstrapInterface;
 
 class ContainerBootstrap implements BootstrapInterface
 {
     public function bootstrap($app)
     {
-        $container = \Yii::$container;
+        $container = Yii::$container;
 
         $container->setSingleton(EventDispatcher::class, DummyEventDispatcher::class);
+
+        $container->setSingleton(EntityManager::class, function () use ($app) {
+            $config = new Configuration();
+
+            $config->setProxyDir(Yii::getAlias('@runtime/doctrine/proxy'));
+            $config->setProxyNamespace('Proxies');
+            $config->setAutoGenerateProxyClasses(!YII_ENV_PROD);
+
+            $cache = YII_ENV_PROD ? new FilesystemCache(Yii::getAlias('@runtime/doctrine/cache')) : new ArrayCache();
+
+            $config->setMetadataCacheImpl($cache);
+            $config->setQueryCacheImpl($cache);
+
+            $config->setMetadataDriverImpl(new SimplifiedYamlDriver([]));
+
+            $evm = new EventManager();
+
+            $evm->addEventSubscriber(new TablePrefixSubscriber($app->db->tablePrefix));
+
+            return EntityManager::create(['pdo' => $app->db->pdo], $config, $evm);
+        });
     }
 }
---
+++ doctrine/listeners/TablePrefixSubscriber.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace app\doctrine\listeners;
+
+use Doctrine\Common\EventSubscriber;
+use Doctrine\ORM\Event\LoadClassMetadataEventArgs;
+use Doctrine\ORM\Events;
+use Doctrine\ORM\Mapping\ClassMetadata;
+
+class TablePrefixSubscriber implements EventSubscriber
+{
+    protected $prefix = '';
+
+    public function __construct($prefix)
+    {
+        $this->prefix = (string) $prefix;
+    }
+
+    public function getSubscribedEvents()
+    {
+        return array(
+            Events::loadClassMetadata,
+        );
+    }
+
+    public function loadClassMetadata(LoadClassMetadataEventArgs $eventArgs)
+    {
+        /** @var \Doctrine\ORM\Mapping\ClassMetadata $classMetadata */
+        $classMetadata = $eventArgs->getClassMetadata();
+
+        if (!$classMetadata->isInheritanceTypeSingleTable() || $classMetadata->getName() === $classMetadata->rootEntityName) {
+            $classMetadata->setPrimaryTable([
+                'name' => $this->prefix . $classMetadata->getTableName(),
+            ]);
+        }
+
+        foreach ($classMetadata->getAssociationMappings() as $fieldName => $mapping) {
+            if ($mapping['type'] === ClassMetadata::MANY_TO_MANY && $mapping['isOwningSide']) {
+                $mappedTableName = $mapping['joinTable']['name'];
+                $classMetadata->associationMappings[$fieldName]['joinTable']['name'] = $this->prefix . $mappedTableName;
+            }
+        }
+    }
+}
